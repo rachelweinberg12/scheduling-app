c576a91e125464c0f0cd06a33d456e6d202bdb59
rachelweinberg12 <rachel.weinberg12@gmail.com>
2025-09-13 16:35:03 -0700

    Add constants for extra feature toggles

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,4 +1,8 @@
-import { CakeIcon, UserGroupIcon } from "@heroicons/react/24/outline";
+import {
+  CakeIcon,
+  FaceSmileIcon,
+  UserGroupIcon,
+} from "@heroicons/react/24/outline";
 import { ForwardRefExoticComponent, RefAttributes, SVGProps } from "react";
 
 export type NavItem = {
@@ -16,7 +20,7 @@ export const CONSTS = {
   TITLE: "Example Conference Weekend",
   DESCRIPTION:
     "An Example Conference is happening from August 23 - 25, 2024 at Lighthaven. Check out the schedules for the main event and afterparty below!",
-  MULTIPLE_EVENTS: true,
+  MULTIPLE_EVENTS: false,
   // If you have multiple events, add your events to the nav bar below
   // If you only have one event, you can leave the array empty
   // Find available icons at https://heroicons.com/
@@ -24,4 +28,6 @@ export const CONSTS = {
     { name: "Conference", href: "/Conference", icon: UserGroupIcon },
     { name: "After Party", href: "/After-Party", icon: CakeIcon },
   ] as NavItem[],
+  CLOSED_SESSIONS: false,
+  PROPOSALS: false,
 };
